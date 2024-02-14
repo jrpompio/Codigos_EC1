f9610d92a8e4c830df508fa2fe4b757d4eef4721
jrpompio <junior.ruiz@ucr.ac.cr>
2024-02-13 22:18:21 -0600

    quiz8

--- quiz8.s
+++ quiz8.s
@@ -7,17 +7,18 @@ li $v0, 10
 syscall
 
 MUL:
-addi $t0, $a1, 0  #a
-addi $t1, $a0, 0  #b
-addi $t2, $0, 0   #i
-addi $t4, $0, 0   #m
+addi $t0, $a1, 0  # $a1 = $t0 = a
+addi $t1, $a0, 0  # $a1 = $t1 = b
+addi $t2, $0, 0   # $t2 = i = 0
+addi $t4, $0, 0   # $t4 = m = 0
 
 LOOP1:
-add $t4, $t4, $t1
-addi $t2, $t2, 1
-slt $t3, $t2, $t0
-bne $t3, $0, LOOP1
+add $t4, $t4, $t1   # m = m + b
+addi $t2, $t2, 1    # i = i + 1
 
-add $v0, $t4, $0
+slt $t3, $t2, $t0   # $t3 = 1 si t2 < t0 
+bne $t3, $0, LOOP1  # se repite si t3 != 0
 
-jr $ra 
+add $v0, $t4, $0    # return
+
+jr $ra
