db474bb331dc3c5d6098aed0b60f9969231fb4d8
jrpompio <junior.ruiz@ucr.ac.cr>
2024-02-25 23:34:51 -0600

    qs

--- quicksort.s
+++ quicksort.s
@@ -7,23 +7,11 @@ lista: .word 1, 10, 20, 45, -1, -100, 100, -10, 56, -989, 1000, 0
 main:
 
 
-la $a0, lista
+la $a1, lista
+li $a2, 0
 jal QS
 
-la $t0, lista
-for:
-lw $t1, 0($t0)
-beq $t1, $0, finalizar
-add $a0, $0, $t1
-addi $v0, $0, 1
-syscall
-addi $t0, $t0, 4
-addi $v0, $0, 11
-addi $a0, $0, 32
-syscall
 
-j for
-finalizar:
 
 
 addi $v0, $0, 10
@@ -33,20 +21,25 @@ syscall
 
 QS:
 
-add $t0, $0, $a0               # cargando direccion de valor inicial a temporal
+add $t0, $0, $a1               # cargando direccion de valor inicial a temporal
 addi $t9, $0, 0                # iniciando contador en cero
 
 contador:
 
 lw $t3, 0($t0)                 # Cargando valor
-beq $t3, $0, fin_contador      # Si valor = cero, termina
+beq $t3, $a2, fin_contador      # Si valor = valor final, termina
 addi $t9, $t9, 1               # contador +1
 addi $t0, $t0, 4               # siguiente valor
 j contador                     
 fin_contador:
 beq $t9, $0, terminar
-
-
+addi $t9, $t9, -1              # t9, valor del ultimo elemento
+# SELECCION DE PIVOTE
+add $t0, $0, $a1               # cargando direccion de valor inicial a temporal
+sll $t9, $t9, 2
+add $t0, $t0, $t9
+lw $a0, 0x00400020
+lw $t8, 0($t0)
 
 
 
